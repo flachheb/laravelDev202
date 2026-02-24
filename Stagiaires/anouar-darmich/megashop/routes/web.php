3b73e6940dab427ed12d1dffb27e1845dde4d21d
anouar <anouardar2003@gmail.com>
2026-02-24 08:41:03 +0000

    project complet

---
+++ Stagiaires/anouar-darmich/megashop/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\ProductController;
+
+Route::get('/', [ShopController::class, 'index']);
+Route::get('/contact', [ShopController::class, 'contact']);
+Route::get('/cgv', [ShopController::class, 'cgv']);
+
+Route::get('/categorie/{slug}', [ProductController::class, 'category']);
+Route::get('/produit/{id}', [ProductController::class, 'show']);
